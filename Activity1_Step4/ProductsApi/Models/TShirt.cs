513a23849094213acd7901b1ab3a7df6b77959a9
Daniel Bass <danbass@googlemail.com>
2018-10-28 11:22:45 +0000

    Fixed structure for Lesson One

---
+++ Activity1_Step4/ProductsApi/Models/TShirt.cs
@@ -0,0 +1,11 @@
+namespace ProductsApi.Models {
+    public class TShirt {
+        public string Name { get; set; }
+
+        public string Size { get; set; }
+
+        public string Colour { get; set; }
+
+        public string Id { get; set; }
+    }
+}
