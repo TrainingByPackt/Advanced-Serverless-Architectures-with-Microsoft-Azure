3ada2c32781239aff948653a1365960a2e0a4440
DeepBass <danbass8@googlemail.com>
2018-11-16 19:10:43 +0000

    wip

--- Lesson2/Exercise2.5/OrderDurableFunctions/Orchestrators/OrchestrateOrderProcessing.cs
+++ Lesson2/Exercise2.5/OrderDurableFunctions/Orchestrators/OrchestrateOrderProcessing.cs
@@ -1,5 +1,13 @@
+using System;
+using Microsoft.Azure.WebJobs;
+using OrderDurableFunctions.Models;
+
 namespace OrderDurableFunctions {
     public class OrchestrateOrderProcessing {
-        
+        [FunctionName("OrchestrateOrderProcessing")]
+        public static void Run([OrchestrationTrigger] DurableOrchestrationContext context){
+            var input = context.GetInput<Order>();
+            Console.WriteLine(input);
+        }
     }
 }
